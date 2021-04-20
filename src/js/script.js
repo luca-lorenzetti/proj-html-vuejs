a0fe6261b34550f398cd00e025be0f15beba0569
luca <luca.lorenzetti0@gmail.com>
2021-04-20 09:28:32 +0200

    Inserimento terza e quarta section e modifica alla navbar menu header

--- src/js/script.js
+++ src/js/script.js
@@ -2,9 +2,76 @@
     el: '#root',
     data: {
 
+        navbar:[
+          {
+            nameItem: "home",
+            active: true,
+            icon: false,
+            pathIcon: null,
+            new: false
+          },
+          {
+            nameItem: "culinary history",
+            active: false,
+            icon: false,
+            pathIcon: null,
+            new: false
+          },
+          {
+            nameItem: "our team",
+            active: false,
+            icon: false,
+            pathIcon: null,
+            new: false
+          },
+          {
+            nameItem: "our menu",
+            active: false,
+            icon: false,
+            pathIcon: null,
+            new: false
+          },
+          {
+            nameItem: "takeout",
+            active: false,
+            icon: false,
+            pathIcon: null,
+            new: true
+          },
+          {
+            nameItem: "bulletin",
+            active: false,
+            icon: false,
+            pathIcon: null,
+            new: false
+          },
+          {
+           
+            nameItem: "reservations",
+            active: false,
+            icon: false,
+            pathIcon: null,
+            new: false
+          },
+          {
+            nameItem: "",
+            active: false,
+            icon: true,
+            pathIcon: "fas fa-shopping-cart",
+            new: false
+          }
+      
+        ]
     },
     methods: {
 
+        removeActiveFromMenuHeader(){
+          this.navbar.forEach( (elem) => {
+            if(elem.active){
+              elem.active = false;
+            }
+          });
+        }
     },
 
   })
