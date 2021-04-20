becf739ae4874f26eb4ed37663cc7e33456c4a7c
luca <luca.lorenzetti0@gmail.com>
2021-04-20 11:22:35 +0200

    Modificate sezioni intermedie

--- src/js/script.js
+++ src/js/script.js
@@ -61,6 +61,52 @@
             new: false
           }
       
+        ],
+        news:[
+          {
+            title: 'the best cofee in town',
+            pathBg: 'src/img/blog92x-400x458.jpg',
+            author: 'Admin',
+            category: "News",
+            date: 'January 20th, 2020',
+            iconPath: 'fas fa-link'
+          },
+          {
+            title: 'discover our new menu',
+            pathBg: 'src/img/blog72x-400x458.jpg',
+            author: 'Admin',
+            category: "News",
+            date: 'January 20th, 2020',
+            iconPath: 'fas fa-link'
+          },
+          {
+            title: 'we now accept square!',
+            pathBg: 'src/img/blog82x-400x458.jpg',
+            author: 'Admin',
+            category: "News",
+            date: 'January 20th, 2020',
+            iconPath: 'fas fa-link'
+          }
+        ],
+        interSection: [
+          {
+            pathBg: "src/img/slider32x.jpg",
+            title: "the best table in town",
+            subTitle: "fine dining experience",
+            content: "Pellentesque vitae viverra risus, sagittis.Venenatis ridiculus scelerisque nisi in urna nulla. Sit tempor a et nisl, ac felis.",
+            iconButton: false,
+            iconPath: null,
+            textButton: "explore the menu"
+          },
+          {
+            pathBg: "src/img/slider72x-scaled.jpg",
+            title: "the best table in town",
+            subTitle: "enjoy your meal at home",
+            content: "Pellentesque vitae viverra risus, sagittis.Venenatis ridiculus scelerisque nisi in urna nulla. Sit tempor a et nisl, ac felis.",
+            iconButton: true,
+            iconPath: "fas fa-car",
+            textButton: "view take out menu"
+          }
         ]
     },
     methods: {
